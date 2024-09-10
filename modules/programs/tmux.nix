43436f07ccab27d4576990f35d7cb81cee3f72f6
Grayton Ward <graytonio.ward@gmail.com>
2024-09-09 23:31:26 -0400

    Enabled dracula tmux config

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -5,6 +5,19 @@
     terminal = "tmux-256color";
     shell = "${pkgs.fish}/bin/fish";
 
+    plugins = with pkgs;
+    [
+      {
+        plugin = tmuxPlugins.dracula;
+        extraConfig = ''
+          set -g @dracula-plugins 'git'
+          set -g @dracula-show-powerline true
+          set -g @dracula-show-flags true
+          set -g @dracula-show-left-icon session
+        '';
+      }
+    ];
+
     extraConfig = ''
       bind "|" split-window -h -c "#{pane_current_path}"
       bind "\\" split-window -fh -c "#{pane_current_path}"
