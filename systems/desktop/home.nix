ae78c1b5f6e1c479765f0d53aaf249c90054b186
Grayton Ward <graytonio.ward@gmail.com>
2024-09-18 16:32:43 -0400

    feat: add desktop home manager

---
+++ systems/desktop/home.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, ... }: {
+  home.username = "graytonio";
+  home.homeDirectory = "/home/graytonio";
+
+  imports =
+    [ 
+      ../../modules/shell 
+      ../../modules/programs 
+      ../../modules/gui 
+      ../../modules/programming 
+      ../../modules/gaming
+    ];
+
+  xresources.properties = {
+    "Xcursor.size" = 16;
+    "Xft.dpi" = 172;
+  };
+
+  home.packages = with pkgs; [ which glow ];
+
+  programs.git = {
+    enable = true;
+    userName = "Grayton Ward";
+    userEmail = "graytonio.ward@gmail.com";
+  };
+
+  home.stateVersion = "24.05";
+  programs.home-manager.enable = true;
+}
