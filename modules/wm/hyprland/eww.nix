d0404f798d92f3b83b328f252c6e34b61c4936eb
Grayton Ward <graytonio.ward@gmail.com>
2024-10-15 15:20:49 -0400

    feat: eww notifications

--- modules/wm/hyprland/eww.nix
+++ modules/wm/hyprland/eww.nix
@@ -1,6 +1,11 @@
 {pkgs, ...}:
 {
-  home.packages = [ pkgs.eww ];
+  home.packages = [ 
+    pkgs.eww
+  
+    pkgs.python311Packages.dbus-python
+    pkgs.python311Packages.pygobject3
+  ];
 
   home.file.".config/eww/".source = ./eww;
 }
