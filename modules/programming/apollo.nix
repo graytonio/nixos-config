7adcc2eb8301ca06234b9a62aabf8a5a32c65e13
Grayton Ward <graytonio.ward@gmail.com>
2025-04-30 12:57:49 -0400

    feat: add oras cli

--- modules/programming/apollo.nix
+++ modules/programming/apollo.nix
@@ -3,6 +3,7 @@
     pkgs.rover
     pkgs.zulu17
     pkgs.google-cloud-sql-proxy
+    pkgs.oras
     (pkgs.google-cloud-sdk.withExtraComponents [
     	pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin
     ])
